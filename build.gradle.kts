ec5291306dcab1f7713196cfb8dfa4c303a94ef8
박주원 <qkrwndnjs7308@gmail.com>
2025-04-30 09:19:00 +0900

    feat ( #2 ) : detekt yml 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,13 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+
 plugins {
-    kotlin("jvm") version "1.9.25"
-    kotlin("plugin.spring") version "1.9.25"
+    kotlin("jvm") version "1.9.23"
+    kotlin("plugin.spring") version "1.9.23"
     id("org.springframework.boot") version "3.4.4"
     id("io.spring.dependency-management") version "1.1.7"
     id("org.jlleitschuh.gradle.ktlint").version("12.1.1")
+    id("io.gitlab.arturbosch.detekt") version "1.23.6"
 }
 
 group = "hs.kr.entrydsm"
@@ -20,6 +24,7 @@ repositories {
 }
 
 dependencies {
+
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
@@ -27,12 +32,29 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
-kotlin {
-    compilerOptions {
-        freeCompilerArgs.addAll("-Xjsr305=strict")
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "17"
     }
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+detekt {
+    config.setFrom(files("detekt.yml"))
+    buildUponDefaultConfig = false // yml에서 설정한 룰만 허용
+    parallel = true // 병렬 실행으로 성능 최적화
+}
+
+tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+    reports {
+        html.required.set(true) // html 보고서만 생성되게 설정
+        xml.required.set(false)
+        txt.required.set(false)
+    }
+
+    jvmTarget = ("17") // Detekt가 사용하는 JVM 타겟을 Java 17로 지정
 }
