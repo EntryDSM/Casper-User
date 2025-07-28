f79374e23efef69fae9084055d55691a1320c286
박주원 <qkrwndnjs7308@gmail.com>
2025-07-28 15:10:43 +0900

    feat ( #14 ) : adapter/out mapper 추가

---
+++ casper-user/src/main/kotlin/hs/kr/entrydsm/user/domain/admin/adapter/out/mapper/AdminMapper.kt
@@ -0,0 +1,16 @@
+package hs.kr.entrydsm.user.domain.admin.adapter.out.mapper
+
+import hs.kr.entrydsm.user.domain.admin.adapter.out.AdminJpaEntity
+import hs.kr.entrydsm.user.domain.admin.model.Admin
+import hs.kr.entrydsm.user.global.mapper.GenericMapper
+import org.mapstruct.Mapper
+
+/**
+ * Admin 도메인 모델과 AdminJpaEntity 간의 변환을 담당하는 매퍼 클래스입니다.
+ */
+@Mapper
+abstract class AdminMapper : GenericMapper<AdminJpaEntity, Admin> {
+    abstract override fun toEntity(model: Admin): AdminJpaEntity
+
+    abstract override fun toModel(entity: AdminJpaEntity?): Admin?
+}
