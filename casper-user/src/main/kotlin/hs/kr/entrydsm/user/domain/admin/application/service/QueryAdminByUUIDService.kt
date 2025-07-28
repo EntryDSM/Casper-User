c1f7353c424f3d3d7d2b6bb72f56af41d3959f3a
박주원 <qkrwndnjs7308@gmail.com>
2025-07-28 15:09:03 +0900

    feat ( #14 ) : application service 추가

---
+++ casper-user/src/main/kotlin/hs/kr/entrydsm/user/domain/admin/application/service/QueryAdminByUUIDService.kt
@@ -0,0 +1,28 @@
+package hs.kr.entrydsm.user.domain.admin.application.service
+
+import hs.kr.entrydsm.user.domain.admin.adapter.`in`.web.dto.response.InternalAdminResponse
+import hs.kr.entrydsm.user.domain.admin.application.port.`in`.QueryAdminByUUIDUseCase
+import hs.kr.entrydsm.user.domain.admin.application.port.out.QueryAdminPort
+import hs.kr.entrydsm.user.domain.admin.exception.AdminNotFoundException
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.util.UUID
+
+/**
+ * UUID로 관리자 조회 비즈니스 로직을 처리하는 서비스 클래스입니다.
+ */
+@Service
+class QueryAdminByUUIDService(
+    private val queryAdminPort: QueryAdminPort,
+) : QueryAdminByUUIDUseCase {
+    /**
+     * UUID를 이용하여 관리자 정보를 조회합니다.
+     */
+    @Transactional(readOnly = true)
+    override fun queryByUUID(adminId: UUID): InternalAdminResponse {
+        val admin = queryAdminPort.findById(adminId) ?: throw AdminNotFoundException
+        return InternalAdminResponse(
+            id = admin.id!!,
+        )
+    }
+}
