93f031cd6651fff96e2a8bb0d6b083829b45bd5f
박주원 <qkrwndnjs7308@gmail.com>
2025-07-29 17:57:37 +0900

    refactor ( #14 ) : mapstruct가 자동 구현할 수 있도록 componentModel = "spring" 추가

--- casper-user/src/main/kotlin/hs/kr/entrydsm/user/domain/admin/adapter/out/mapper/AdminMapper.kt
+++ casper-user/src/main/kotlin/hs/kr/entrydsm/user/domain/admin/adapter/out/mapper/AdminMapper.kt
@@ -8,7 +8,7 @@ import org.mapstruct.Mapper
 /**
  * Admin 도메인 모델과 AdminJpaEntity 간의 변환을 담당하는 매퍼 클래스입니다.
  */
-@Mapper
+@Mapper(componentModel = "spring")
 abstract class AdminMapper : GenericMapper<AdminJpaEntity, Admin> {
     abstract override fun toEntity(model: Admin): AdminJpaEntity
 
