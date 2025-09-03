640fd307c77ddbc23751e582c53f099a03208fbc
박주원 <qkrwndnjs7308@gmail.com>
2025-09-03 17:37:11 +0900

    feat ( #33 ) : kafkaProducerConfig 추가

---
+++ casper-user/src/main/kotlin/hs/kr/entrydsm/user/infrastructure/kafka/configuration/KafkaProducerConfig.kt
@@ -0,0 +1,50 @@
+package hs.kr.entrydsm.user.infrastructure.kafka.configuration
+
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.serialization.StringSerializer
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.support.serializer.JsonSerializer
+
+@Configuration
+class KafkaProducerConfig(
+    private val kafkaProperty: KafkaProperty
+) {
+
+    @Bean
+    fun deleteAllTableProducerFactory(): DefaultKafkaProducerFactory<String, Unit> {
+        return DefaultKafkaProducerFactory(producerConfig())
+    }
+
+    @Bean
+    fun deleteAllTableKafkaTemplate(): KafkaTemplate<String, Unit> {
+        return KafkaTemplate(deleteAllTableProducerFactory())
+    }
+
+    @Bean
+    fun deleteUserProducerFactory(): DefaultKafkaProducerFactory<String, Long> {
+        return DefaultKafkaProducerFactory(producerConfig())
+    }
+
+    @Bean
+    fun deleteUserKafkaTemplate(): KafkaTemplate<String, Long> {
+        return KafkaTemplate(deleteUserProducerFactory())
+    }
+
+
+    private fun producerConfig(): Map<String, Any> {
+        return mapOf(
+            ProducerConfig.BOOTSTRAP_SERVERS_CONFIG to kafkaProperty.serverAddress,
+            ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java,
+            ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG to JsonSerializer::class.java,
+            "security.protocol" to "SASL_PLAINTEXT",
+            "sasl.mechanism" to "SCRAM-SHA-512",
+            "sasl.jaas.config" to
+                    "org.apache.kafka.common.security.scram.ScramLoginModule required " +
+                    "username=\"${kafkaProperty.confluentApiKey}\" " +
+                    "password=\"${kafkaProperty.confluentApiSecret}\";"
+        )
+    }
+}
