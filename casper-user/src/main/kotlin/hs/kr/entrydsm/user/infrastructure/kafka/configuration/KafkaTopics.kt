d43a292a5b66ffe161be61b5502e26fe34deb04e
박주원 <qkrwndnjs7308@gmail.com>
2025-09-03 19:51:10 +0900

    feat ( #33 ) : topics 추가

--- casper-user/src/main/kotlin/hs/kr/entrydsm/user/infrastructure/kafka/configuration/KafkaTopics.kt
+++ casper-user/src/main/kotlin/hs/kr/entrydsm/user/infrastructure/kafka/configuration/KafkaTopics.kt
@@ -4,4 +4,8 @@ object KafkaTopics {
     const val DELETE_USER = "delete-user"
     const val DELETE_ALL_TABLE = "delete-all-table"
     const val CREATE_APPLICATION = "create-application"
+
+    // Choreography 이벤트들
+    const val USER_RECEIPT_CODE_UPDATE_COMPLETED = "user-receipt-code-update-completed"
+    const val USER_RECEIPT_CODE_UPDATE_FAILED = "user-receipt-code-update-failed"
 }
