1dcada7986fe990a5ceeda810f339fe85b67be47
박주원 <qkrwndnjs7308@gmail.com>
2025-09-03 17:40:15 +0900

    feat ( #33 ) : kafkaTopic 추가

---
+++ casper-user/src/main/kotlin/hs/kr/entrydsm/user/infrastructure/kafka/configuration/KafkaTopics.kt
@@ -0,0 +1,7 @@
+package hs.kr.entrydsm.user.infrastructure.kafka.configuration
+
+object KafkaTopics {
+    const val DELETE_USER = "delete-user"
+    const val DELETE_ALL_TABLE = "delete-all-table"
+    const val CREATE_APPLICATION = "create-application"
+}
