6b9b9ab56ad6ce5ab87a466a15182ecb17d8c2a5
박주원 <qkrwndnjs7308@gmail.com>
2025-10-08 17:19:48 +0900

    chore : 토큰 재발급 로직 수정

--- casper-user/src/main/kotlin/hs/kr/entrydsm/user/domain/admin/application/service/AdminTokenRefreshService.kt
+++ casper-user/src/main/kotlin/hs/kr/entrydsm/user/domain/admin/application/service/AdminTokenRefreshService.kt
@@ -1,6 +1,9 @@
 package hs.kr.entrydsm.user.domain.admin.application.service
 
 import hs.kr.entrydsm.user.domain.admin.application.port.`in`.AdminTokenRefreshUseCase
+import hs.kr.entrydsm.user.domain.user.adapter.out.domain.UserInfo
+import hs.kr.entrydsm.user.domain.user.adapter.out.persistence.repository.UserInfoRepository
+import hs.kr.entrydsm.user.global.security.jwt.JwtProperties
 import hs.kr.entrydsm.user.global.security.jwt.JwtTokenProvider
 import hs.kr.entrydsm.user.global.utils.token.dto.TokenResponse
 import org.springframework.stereotype.Service
@@ -12,10 +15,24 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 class AdminTokenRefreshService(
     private val jwtTokenProvider: JwtTokenProvider,
+    private val userInfoRepository: UserInfoRepository,
+    private val jwtProperties: JwtProperties,
 ) : AdminTokenRefreshUseCase {
     /**
      * 관리자의 리프레시 토큰을 이용하여 새로운 액세스 토큰을 발급합니다.
      */
     @Transactional
-    override fun refresh(refreshToken: String): TokenResponse = jwtTokenProvider.reIssue(refreshToken)
+    override fun refresh(refreshToken: String): TokenResponse {
+        val tokenResponse = jwtTokenProvider.reIssue(refreshToken)
+        
+        val userInfo = UserInfo(
+            token = tokenResponse.accessToken,
+            userId = jwtTokenProvider.getSubjectWithExpiredCheck(tokenResponse.accessToken),
+            userRole = jwtTokenProvider.getRole(tokenResponse.accessToken),
+            ttl = jwtProperties.accessExp,
+        )
+        userInfoRepository.save(userInfo)
+        
+        return tokenResponse
+    }
 }
