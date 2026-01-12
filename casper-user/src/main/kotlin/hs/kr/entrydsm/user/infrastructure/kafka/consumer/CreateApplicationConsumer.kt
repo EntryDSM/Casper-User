5416dd3be2a915e753280ea842587441794d0089
coehgns <modooboiroo@gmail.com>
2026-01-12 19:56:15 +0900

    chore : CreateApplicationConsumer 재수정

--- casper-user/src/main/kotlin/hs/kr/entrydsm/user/infrastructure/kafka/consumer/CreateApplicationConsumer.kt
+++ casper-user/src/main/kotlin/hs/kr/entrydsm/user/infrastructure/kafka/consumer/CreateApplicationConsumer.kt
@@ -22,8 +22,7 @@ class CreateApplicationConsumer(
         containerFactory = "kafkaListenerContainerFactory",
     )
     fun execute(message: String) {
-        val jsonData = mapper.readValue(message, String::class.java)
-        val createApplicationEvent = mapper.readValue(jsonData, CreateApplicationEvent::class.java)
+        val createApplicationEvent = mapper.readValue(message, CreateApplicationEvent::class.java)
         changeReceiptCodeUseCase.changeReceiptCode(createApplicationEvent.userId, createApplicationEvent.receiptCode)
     }
 }
