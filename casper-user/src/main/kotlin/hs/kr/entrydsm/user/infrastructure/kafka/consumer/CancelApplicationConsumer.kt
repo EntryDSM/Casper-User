17475164de773198ecc21c7917a5086553e92e90
coehgns <modooboiroo@gmail.com>
2026-01-13 17:03:58 +0900

    feat ( #36 ): CancelApplicationConsumer

---
+++ casper-user/src/main/kotlin/hs/kr/entrydsm/user/infrastructure/kafka/consumer/CancelApplicationConsumer.kt
@@ -0,0 +1,24 @@
+package hs.kr.entrydsm.user.infrastructure.kafka.consumer
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import hs.kr.entrydsm.user.domain.user.application.port.`in`.DeleteReceiptCodeUseCase
+import hs.kr.entrydsm.user.infrastructure.kafka.configuration.KafkaTopics
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.stereotype.Component
+
+@Component
+class CancelApplicationConsumer(
+    private val mapper: ObjectMapper,
+    private val deleteReceiptCodeUseCase: DeleteReceiptCodeUseCase
+) {
+
+    @KafkaListener(
+        topics = [KafkaTopics.CANCEL_SUBMITTED_APPLICATION],
+        groupId = "update-user",
+        containerFactory = "kafkaListenerContainerFactory",
+    )
+    fun execute(message: String) {
+        val receiptCode = mapper.readValue(message, Long::class.java)
+        deleteReceiptCodeUseCase.deleteReceiptCode(receiptCode)
+    }
+}
